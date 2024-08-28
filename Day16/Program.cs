dc010b73e805108c20cf722259a211433b9d204c
Jan Repar <jan.repar@outlook.com>
2024-08-28 21:24:29 +0200

    Added day16.

---
+++ Day16/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Day16/Valve.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16
+{
+    public class Valve
+    {
+        public string Name { get; set; }
+        public int FlowRate { get; set; }
+        public bool Opened { get; set; }
+        public HashSet<Valve> tunnelsTo { get; set; }
+    }
+}
